a19e1eb1a3363075f3e30d03858ad26455281933
Martin Schade <45048633+schadem@users.noreply.github.com>
2022-05-25 11:19:36 -0700

    Bump version: 0.0.6 → 0.0.7

--- textractmanifest/__init__.py
+++ textractmanifest/__init__.py
@@ -4,4 +4,4 @@ from .manifest import IDPManifest, Query, IDPManifestSchema, QuerySchema
 
 logging.getLogger('tidpmanifest').addHandler(NullHandler())
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
